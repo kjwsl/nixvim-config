e1d7b2c14e089e0c9c385f08c423193b4c495f03
kjwsl <larpios@protonmail.com>
2024-08-26 12:53:12 +0900

    update: add more keymaps

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -10,7 +10,22 @@
         "n"
       ];
     }
-
+    {
+        key = "<leader>qq";
+      action = "<cmd>confirm qa<cr>";
+      options = {
+        silent = true;
+        desc = "Exit Neovim";
+      };
+    }
+    {
+        key = "<leader>y";
+      action = "\"+y";
+      options = {
+        silent = true;
+        desc = "Yank to Clipboard";
+      };
+    }
   ];
 
 }
